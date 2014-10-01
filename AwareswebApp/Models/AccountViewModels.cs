f03c8ea5e200bf56949c4caf4f3a9268678faef8
Amaury R. Peralta Febles <amauryperalta28@gmail.com>
2014-10-01 02:57:20 -0400

    Revert "Revert "Algunos cambios para el feature de los roles""
    This reverts commit 4a6ffb467feba5ebf16f1d76f3283c4676e2bcf3.

--- AwareswebApp/Models/AccountViewModels.cs
+++ AwareswebApp/Models/AccountViewModels.cs
@@ -1,14 +1,71 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace AwareswebApp.Models
 {
-    public class ExternalLoginConfirmationViewModel
+    #region Manejo de Roles
+    public class SelectUserRolesViewModel
     {
-        [Required]
-        [EmailAddress]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
+        public SelectUserRolesViewModel()
+        {
+            this.Roles = new List<SelectRoleEditorViewModel>();
+        }
+
+
+        // Enable initialization with an instance of ApplicationUser:
+        public SelectUserRolesViewModel(ApplicationUser user)
+            : this()
+        {
+           
+            this.UserName = user.UserName;
+            //this.FirstName = user.FirstName;
+           // this.LastName = user.LastName;
+
+            var Db = new ApplicationDbContext();
+
+            // Add all available roles to the list of EditorViewModels:
+            var allRoles = Db.Roles;
+            foreach (var role in allRoles)
+            {
+                // An EditorViewModel will be used by Editor Template:
+                var rvm = new SelectRoleEditorViewModel(role);
+                this.Roles.Add(rvm);
+            }
+
+            // Set the Selected property to true for those roles for 
+            // which the current user is a member:
+            foreach (var userRole in user.Roles)
+            {
+                
+                
+                var checkUserRole =
+                    this.Roles.Find(r => r.RoleName == userRole.Role.Name);
+                checkUserRole.Selected = true;
+            }
+        }
+
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<SelectRoleEditorViewModel> Roles { get; set; }
     }
+
+    public class SelectRoleEditorViewModel
+    {
+        public SelectRoleEditorViewModel() { }
+        public SelectRoleEditorViewModel(IdentityRole role)
+        {
+            this.RoleName = role.Name;
+        }
+
+        public bool Selected { get; set; }
+
+        [Required]
+        public string RoleName { get; set; }
+    }
+    #endregion 
+    
 
     public class ExternalLoginListViewModel
     {
