4a6ffb467feba5ebf16f1d76f3283c4676e2bcf3
Amaury R. Peralta Febles <amauryperalta28@gmail.com>
2014-10-01 02:57:14 -0400

    Revert "Algunos cambios para el feature de los roles"
    This reverts commit 1632c93b2f57dbacd881b699e7990bf7a30a75f8.

--- AwareswebApp/Models/IdentityModels.cs
+++ AwareswebApp/Models/IdentityModels.cs
@@ -2,22 +2,12 @@
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
-using System.ComponentModel.DataAnnotations;
-using System.Collections.Generic;
 
 namespace AwareswebApp.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        [Required]
-        public string FirstName { get; set; }
-
-        [Required]
-        public string LastName { get; set; }
-
-        [Required]
-        public string Email { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -39,65 +29,4 @@ namespace AwareswebApp.Models
             return new ApplicationDbContext();
         }
     }
-
-    #region Manejo de Roles
-    public class IdentityManager
-    {
-        public bool RoleExists(string name)
-        {
-            var rm = new RoleManager<IdentityRole>(
-                new RoleStore<IdentityRole>(new ApplicationDbContext()));
-            return rm.RoleExists(name);
-        }
-
-
-        public bool CreateRole(string name)
-        {
-            var rm = new RoleManager<IdentityRole>(
-                new RoleStore<IdentityRole>(new ApplicationDbContext()));
-            var idResult = rm.Create(new IdentityRole(name));
-            return idResult.Succeeded;
-        }
-
-
-        public bool CreateUser(ApplicationUser user, string password)
-        {
-            var um = new UserManager<ApplicationUser>(
-                new UserStore<ApplicationUser>(new ApplicationDbContext()));
-            var idResult = um.Create(user, password);
-            return idResult.Succeeded;
-        }
-
-
-        public bool AddUserToRole(string userId, string roleName)
-        {
-            var um = new UserManager<ApplicationUser>(
-                new UserStore<ApplicationUser>(new ApplicationDbContext()));
-            var idResult = um.AddToRole(userId, roleName);
-            return idResult.Succeeded;
-        }
-
-
-        public void ClearUserRoles(string userId)
-        {
-            var um = new UserManager<ApplicationUser>(
-                new UserStore<ApplicationUser>(new ApplicationDbContext()));
-            var user = um.FindById(userId);
-            var currentRoles = new List<IdentityUserRole>();
-
-            
-            
-            currentRoles.AddRange(user.Roles);
-
-            
-            foreach (var role in currentRoles)
-            {
-                //um.RemoveFromRole(userId, role.Role.Name);
-                
-            }
-        }
-
-    }
-
-#endregion
 }
